6dbc17a93ea3d5ce4724ffa15fffc5191b2dde9e
unknown <shilpy.jain88@gmail.com>
2019-08-17 14:38:35 +0530

    A new file

--- test.py
+++ test.py
@@ -4,3 +4,9 @@ def  addone(num):
 if __name__ == "__main__":   # to directly run this file
     print(addone(2))
     
+def square(num):
+   return num**2
+
+if __name__ == "__main__":
+    print(square(2))
+    
